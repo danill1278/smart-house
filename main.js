5f5e0977538bd13acb159a52f00ced91bd133694
alina_laktionova <alinalak18for@gmail.com>
2019-10-29 23:14:47 +0200

    implement inherirance

--- main.js
+++ main.js
@@ -1,120 +1,151 @@
-let SmartKettle = function(name){
-    if(this.setName(name)){
-        this.__name = name;
-    }
-    this.__state = false;
-    this.__modes = {
-        "standart": 100,
-        "tea": 78,
-        "coffee": 85,
-        "porridge": 72,
-        "baby food": 70
-    };
-    this.__currentMode = "standart";
-    this.__maxFullness = 1000;
-    this. __minFullness = 100;
-    this.__currentFullness = 0;
-    this.__currentTemperature = 0;
+let Machine = function(name) {
+  if (this._checkNameValidity(name)) {
+    this._name = name;
+  } else {
+    //default name
+    this._name = "default";
+  }
+  this._state = false;
+};
+Machine.prototype.info = function() {
+  console.log(`
+        name: ${this._name};
+        state: ${this._state};`);
+};
 
-}
+Machine.prototype.getState = function() {
+  return this._state;
+};
 
-SmartKettle.prototype.getInfo = function(){
-    console.log(`
-        name: ${this.__name};
-        state: ${this.__state};
+Machine.prototype.getName = function() {
+  return this._name;
+};
+
+Machine.prototype.setName = function(name) {
+  if (this._checkNameValidity(name)) {
+    this._name = name;
+  }
+};
+
+Machine.prototype._isDeviceOn = function() {
+  if (!this.getState()) {
+    throw new Error("Turn on device, please!");
+  }
+  return true;
+};
+
+Machine.prototype._checkNameValidity = function(name) {
+  if (typeof name !== "string") {
+    throw new Error("Name must be a string");
+  }
+  const regex = /^\w{5,10}/i;
+  const result = name.match(regex);
+  if (result) {
+    this._name = result[0];
+    return true;
+  } else {
+    throw new Error("Name must contain 5-10 characters");
+  }
+};
+
+Machine.prototype.on = function() {
+  this._state = true;
+};
+
+Machine.prototype.off = function() {
+  this._state = false;
+};
+
+Machine.prototype._deleteTimer = function() {
+  clearInterval(this._timer);
+};
+
+const SmartKettle = function(name = "kettle22") {
+  Machine.call(this, name);
+  this.__modes = {
+    standart: 100,
+    tea: 78,
+    coffee: 85,
+    porridge: 72,
+    "baby food": 70
+  };
+  this.__currentMode = "standart";
+  this.__maxFullness = 1000;
+  this.__minFullness = 100;
+  this.__currentFullness = 0;
+  this.__currentTemperature = 0;
+};
+
+SmartKettle.prototype = Object.create(Machine.prototype);
+SmartKettle.prototype.constructor = SmartKettle;
+
+SmartKettle.prototype.info = function() {
+  Machine.prototype.info.call(this);
+  console.log(`
         mode: ${this.__currentMode};
         currentFullness: ${this.__currentFullness};
-    `)
+    `);
+};
+/*SmartKettle.prototype.on = function() {
+  Machine.prototype.on.call(this);
+  console.log("ON");
+};
 
-}
+SmartKettle.prototype.off = function() {
+  Machine.prototype.off.call(this);
+  console.log("OFF");
+};*/
 
-SmartKettle.prototype.getName = function(){
-    return this.__name;
-}
+SmartKettle.prototype.setTemperature = function(value) {
+  if (typeof value == "number" && value >= 10 && value <= 70) {
+    this.__currentTemperature = value;
+  } else {
+    throw new Error("Temperature must be in range from 10 to 70C");
+  }
+};
 
-SmartKettle.prototype.setName = function(value){
-    const regex = /^\w{5,10}/i,
-        result = value.match(regex);
-    if(result !== null) {
-        this.__name = result[0];
-        return true;
-    }
-    else{
-        throw new Error("Name must be a string and contain from 5 to 10 characters");
-    }
-}
+SmartKettle.prototype.changeMode = function(value) {
+  if (this._isDeviceOn() && typeof value == "string" && value in this.__modes) {
+    this.__currentMode = value;
+  }
+};
 
-SmartKettle.prototype.on = function(){
-    this.__state = true;
-    console.log("ON");
-}
-
-SmartKettle.prototype.off = function(){
-    this.__state = false;
-    console.log("OFF");
-}
-
-SmartKettle.prototype.getState = function(){
-    return this.__state;
-}
-
-SmartKettle.prototype.setTemperature = function(value){
-    if(typeof value == "number" && value >= 10 && value <= 70) {
-        this.__currentTemperature = value;
-    }
-    else{
-        throw new Error("Temperature must be in range from 10 to 70C");
-    }
-}
-
-SmartKettle.prototype.changeMode = function(value){
-    if(this.__state === true && typeof value == "string" && this.__modes.hasOwnProperty(value)){
-        this.__currentMode = value;
-    }
-}
-
-SmartKettle.prototype.getCurrentMode = function(){
-    return this.__currentMode;
-}
+SmartKettle.prototype.getCurrentMode = function() {
+  return this.__currentMode;
+};
 
 SmartKettle.prototype.addWater = function(value) {
-    if(typeof(value) == "number"){
-        if((this.__currentFullness + value) >= this.__minFullness &&
-            (this.__currentFullness + value) <= this.__maxFullness){
-            this.__currentFullness += value;
-        }
-        else if((this.__currentFullness + value) < this.__minFullness) {
-            throw new Error("Please, add more water");
-        }
-        else{
-            throw new Error("Please, reduce the amount of water");
-        }
-    }
-    else {
-        throw new Error("Input valid value");
-    }
-}
+  let newAmountOfWater = this.__currentFullness + value;
+  if (typeof value !== "number") {
+    throw new Error("Input valid value");
+  }
+  if (
+    newAmountOfWater >= this.__minFullness &&
+    newAmountOfWater <= this.__maxFullness
+  ) {
+    this.__currentFullness = newAmountOfWater;
+  } else if (newAmountOfWater < this.__minFullness) {
+    throw new Error("Please, add more water");
+  } else {
+    throw new Error("Please, reduce the amount of water");
+  }
+};
 
 SmartKettle.prototype.getCurrentFullness = function() {
-    return this.__currentFullness;
-}
+  return this.__currentFullness;
+};
 
-SmartKettle.prototype.boilWater = function (temperature) {
-    if (this.__state === true && this.__currentFullness) {
-            this.setTemperature(temperature);
-            this.__timer = setInterval(() => {
-                    if(this.__currentTemperature === this.__modes[this.__currentMode]){
-                        this.off();
-                        clearInterval(this.__timer);
-                    } else {
-                    this.__currentTemperature++;
-                    }
-                }, 1000);
-    }
-}
-
-
-let mk =new SmartKettle("alina18181818181818");
-
-
-
+SmartKettle.prototype.boilWater = function(temperature) {
+  if (this._isDeviceOn() && this.__currentFullness) {
+    this.setTemperature(temperature);
+    this._timer = setInterval(() => {
+      if (this.__currentTemperature === this.__modes[this.__currentMode]) {
+        this.off();
+        this._deleteTimer();
+      } else {
+        this.__currentTemperature++;
+        this.__currentFullness -= 2;
+      }
+    }, 1000);
+  }
+};
